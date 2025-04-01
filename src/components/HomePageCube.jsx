42789f552e0bf8d797ab46166003c4579ec5786c
melihdr <meljhdurmus@gmail.com>
2025-04-02 02:12:32 +0300

    message

--- src/components/HomePageCube.jsx
+++ src/components/HomePageCube.jsx
@@ -1,5 +1,4 @@
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three";
 import halftoneVertexShader from "../shaders/halftone/vertex.glsl";
--- src/components/HomePageProjects.jsx
+++ src/components/HomePageProjects.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import SingleProject from "./SingleProject";
-import three_js_image from "/images/threejs.png";
 import physics_image_1 from "/images/physics.jpg";
-import physics_image_2 from "/images/physics2.jpg";
-import physics_image_3 from "/images/physics3.jpg";
 
 function HomePageProjects() {
   return (
