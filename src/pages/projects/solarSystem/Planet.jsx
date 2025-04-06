32f591ac5a6964312aad04cc65fe6b504d3c8afc
melihdr <meljhdurmus@gmail.com>
2025-04-06 06:31:21 +0300

    done

--- src/pages/projects/solarSystem/Planet.jsx
+++ src/pages/projects/solarSystem/Planet.jsx
@@ -1,7 +1,14 @@
 import React, { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 
-function Planet({ position, orbitRadius, rotateSpeed, rotateOffset, texture }) {
+function Planet({
+  position,
+  orbitRadius,
+  rotateSpeed,
+  rotateOffset,
+  texture,
+  sphereRadius = 1,
+}) {
   const sphereRef = useRef();
 
   let xPosition;
@@ -11,14 +18,17 @@ function Planet({ position, orbitRadius, rotateSpeed, rotateOffset, texture }) {
     sphereRef.current.position.x =
       Math.cos(rotateOffset + state.clock.elapsedTime * rotateSpeed) *
       orbitRadius;
+
     sphereRef.current.position.z =
       Math.sin(rotateOffset + state.clock.elapsedTime * rotateSpeed) *
       orbitRadius;
+
+    sphereRef.current.rotation.y += 0.01;
   });
 
   return (
     <mesh receiveShadow ref={sphereRef} position={position}>
-      <sphereGeometry />
+      <sphereGeometry args={[sphereRadius, 16, 16]} />
       <meshStandardMaterial map={texture} />
     </mesh>
   );
--- src/pages/projects/solarSystem/SolarSystem.jsx
+++ src/pages/projects/solarSystem/SolarSystem.jsx
@@ -8,9 +8,12 @@ import {
 import Planet from "./Planet";
 
 function SolarSystem() {
-  const [cameraPos, setCameraPos] = useState([20, 20, 20]);
+  const [cameraPos, setCameraPos] = useState([45, 45, 45]);
+  const sunRef = useRef();
 
-  const cameraRef = useRef();
+  let speedCoefficient = 0.01;
+  let radiusCoefficient = (1 / 6371) * 0.3;
+  let positionCoefficient = 1.5;
 
   const mercuryTexture = useTexture("./images/planets/2k_mercury.jpg");
   const venusTexture = useTexture("./images/planets/2k_venus_surface.jpg");
@@ -20,83 +23,109 @@ function SolarSystem() {
   const saturnTexture = useTexture("./images/planets/2k_saturn.jpg");
   const uranusTexture = useTexture("./images/planets/2k_uranus.jpg");
   const neptuneTexture = useTexture("./images/planets/2k_neptune.jpg");
+  const sunTexture = useTexture("./images/planets/2k_sun.jpg");
+
+  const mercuryRadius = 2430;
+  const venusRadius = 6051;
+  const earthRadius = 6371;
+  const marsRadius = 3389;
+  const jupiterRadius = 69911 * 0.65;
+  const saturnRadius = 58232 * 0.65;
+  const uranusRadius = 25362;
+  const neptuneRadius = 24622;
+
+  useFrame((state, deltaTime) => {
+    sunRef.current.rotation.y += 0.01;
+  });
 
   return (
     <>
-      <axesHelper args={[5]} />
-      <PerspectiveCamera ref={cameraRef} makeDefault position={cameraPos} />
+      <PerspectiveCamera makeDefault position={cameraPos} />
 
       <color args={["black"]} attach="background" />
       <OrbitControls />
 
-      <ambientLight intensity={0.2} />
+      <ambientLight intensity={0.3} />
       <pointLight
         position={[0, 0, 0]}
-        intensity={100}
-        color="yellow"
+        intensity={1400}
+        distance={100}
+        color="orange"
         castShadow
       />
 
-      <mesh>
+      <mesh ref={sunRef}>
         <sphereGeometry />
-        <meshStandardMaterial color={"yellow"} emissive="yellow" />
+        <meshStandardMaterial
+          emissive="yellow"
+          emissiveMap={sunTexture}
+          emissiveIntensity={1.5}
+        />
       </mesh>
 
       <Planet
-        position={[5, 0, 0]}
+        position={[0.39 * positionCoefficient, 0, 0]}
         orbitRadius={5}
-        rotateSpeed={1}
-        rotateOffset={3.14}
+        rotateSpeed={48 * speedCoefficient}
+        rotateOffset={Math.random() * Math.PI * 2}
         texture={mercuryTexture}
+        sphereRadius={mercuryRadius * radiusCoefficient}
       />
       <Planet
-        position={[5, 0, 0]}
+        position={[0.72 * positionCoefficient, 0, 0]}
         orbitRadius={10}
-        rotateSpeed={0.5}
-        rotateOffset={0}
+        rotateSpeed={35 * speedCoefficient}
+        rotateOffset={Math.random() * Math.PI * 2}
         texture={venusTexture}
+        sphereRadius={venusRadius * radiusCoefficient}
       />
       <Planet
-        position={[5, 0, 0]}
+        position={[1 * positionCoefficient, 0, 0]}
         orbitRadius={15}
-        rotateSpeed={1}
-        rotateOffset={3.14}
+        rotateSpeed={30 * speedCoefficient}
+        rotateOffset={Math.random() * Math.PI * 2}
         texture={earthTexture}
+        sphereRadius={earthRadius * radiusCoefficient}
       />
       <Planet
-        position={[5, 0, 0]}
+        position={[1.52 * positionCoefficient, 0, 0]}
         orbitRadius={20}
-        rotateSpeed={0.5}
-        rotateOffset={0}
+        rotateSpeed={24 * speedCoefficient}
+        rotateOffset={Math.random() * Math.PI * 2}
         texture={marsTexture}
+        sphereRadius={marsRadius * radiusCoefficient}
       />
       <Planet
-        position={[5, 0, 0]}
+        position={[5.2, 0, 0]}
         orbitRadius={25}
-        rotateSpeed={1}
-        rotateOffset={3.14}
+        rotateSpeed={13 * speedCoefficient}
+        rotateOffset={Math.random() * Math.PI * 2}
         texture={jupiterTexture}
+        sphereRadius={jupiterRadius * radiusCoefficient}
       />
       <Planet
-        position={[5, 0, 0]}
+        position={[9.6 * positionCoefficient, 0, 0]}
         orbitRadius={30}
-        rotateSpeed={0.5}
-        rotateOffset={0}
+        rotateSpeed={9.7 * speedCoefficient}
+        rotateOffset={Math.random() * Math.PI * 2}
         texture={saturnTexture}
+        sphereRadius={saturnRadius * radiusCoefficient}
       />
       <Planet
-        position={[5, 0, 0]}
+        position={[19.2 * positionCoefficient, 0, 0]}
         orbitRadius={35}
-        rotateSpeed={1}
-        rotateOffset={3.14}
+        rotateSpeed={6.8 * speedCoefficient}
+        rotateOffset={Math.random() * Math.PI * 2}
         texture={uranusTexture}
+        sphereRadius={uranusRadius * radiusCoefficient}
       />
       <Planet
-        position={[5, 0, 0]}
+        position={[30 * positionCoefficient, 0, 0]}
         orbitRadius={40}
-        rotateSpeed={0.5}
-        rotateOffset={0}
+        rotateSpeed={5.4 * speedCoefficient}
+        rotateOffset={Math.random() * Math.PI * 2}
         texture={neptuneTexture}
+        sphereRadius={neptuneRadius * radiusCoefficient}
       />
     </>
   );
