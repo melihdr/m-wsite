78dbd9ca055652a404fa6261199cf66fa294fcd9
melihdr <meljhdurmus@gmail.com>
2025-04-07 16:08:20 +0300

    image renewed

--- src/components/HomePageProjects.jsx
+++ src/components/HomePageProjects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SingleProject from "./SingleProject";
 import physics_image_1 from "/images/physics.jpg";
-import solar_system_image from "/images/home_page_projects_links/solar_system.png";
+import solar_system from "/images/home_page_projects/solar_system.png";
 
 function HomePageProjects() {
   return (
@@ -9,7 +9,7 @@ function HomePageProjects() {
       <SingleProject
         name={"solar system"}
         date={"06.04.2025"}
-        image={solar_system_image}
+        image={solar_system}
         link={"/solar_system"}
       />
       <SingleProject image={physics_image_1} link={"/projects"} />
